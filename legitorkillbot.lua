a3e7b97b03ca7e69eaadecc60adc7f9d1321f721
Hxckerskyy <lajmicz2@gmail.com>
2024-07-30 18:53:57 +0200

    Create legitorkillbot.lua

---
+++ legitorkillbot.lua
@@ -0,0 +1,5 @@
+if getgenv().Loader == "Legit" then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hxckerskyy/suicide.lol/main/legit.lua"))()
+elseif getgenv().Loader == "Killbot" then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hxckerskyy/suicide.lol/main/killbot.lua"))()
+end
